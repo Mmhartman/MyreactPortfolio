8d53e96e5b361aeef544786d00d3af20722c5540
Ann <miannmhartman@gmail.com>
2023-05-22 15:32:00 -0700

    Updated About Me Page and Title

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -14,8 +14,8 @@ const Layout = () => {
       <Outlet />
       <span className="tags bottom-tags">
       &lt;/body&gt;
-      <br/>
-      <span className="bottom-tag-html">&lt;/html&gt;</span>
+      {/* <br/> */}
+      {/* <span className="bottom-tag-html">&lt;/html&gt;</span> */}
 
       </span>
       </div>
